35addc9dcdb26a9fd4f9f567a52f4d88788cc8e7
LifeIsOne <didtmdgh258@gmail.com>
2024-07-08 11:45:14 +0900

    React : router 설계

---
+++ src/main/webapp/frontapp/src/pages/post/Home.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import { Pagination } from "react-bootstrap";
+import PostItem from "../../components/PostItem";
+import axios from "axios";
+
+const Home = () => {
+  const [posts, setPosts ] = useState([]);
+
+  useEffect(()=>{
+    apiHome();
+  },[]);
+  
+  async function apiHome(){
+    let response = await axios({
+      url: "http:localhost:8080",
+      method: "GET"
+    });
+
+    console.log("posts", response.data);
+    setPosts(response.data.body);
+  }
+
+  function prev() {}
+  function next() {}
+
+  return (
+    <div>
+      {posts.map(post => (
+        <PostItem key={post.id} id={post.id} title={post.title} />
+      ))}
+      {/* {<PostItem id={1} title={"제목1"} />} */}
+      {<PostItem id={2} title={"제목2"} />}
+      <br />
+      <div className="d-flex justify-content-center">
+        <Pagination>
+          <Pagination.Item onClick={prev} disabled>
+            Prev
+          </Pagination.Item>
+          <Pagination.Item onClick={next}>Next</Pagination.Item>
+        </Pagination>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
