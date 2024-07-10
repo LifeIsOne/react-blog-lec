4e1f171a223ffa7ee758ef729d5c6abff4bb7f67
LifeIsOne <didtmdgh258@gmail.com>
2024-07-10 11:14:58 +0900

    post -> board 변경

--- src/main/webapp/frontapp/src/App.js
+++ src/main/webapp/frontapp/src/App.js
@@ -1,10 +1,10 @@
 import { Container } from "react-bootstrap";
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
-import Detail from "./pages/post/Detail";
-import Home from "./pages/post/Home";
-import SaveForm from "./pages/post/SaveForm";
-import UpdateForm from "./pages/post/UpdateForm";
+import Detail from "./pages/board/Detail";
+import Home from "./pages/board/Home";
+import SaveForm from "./pages/board/SaveForm";
+import UpdateForm from "./pages/board/UpdateForm";
 import JoinForm from "./pages/user/JoinForm";
 import LoginForm from "./pages/user/LoginForm";
 
@@ -14,12 +14,13 @@ function App() {
       <Header />
       <Container>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/"         element={<Home />} />
           <Route path="/saveForm" element={<SaveForm />} />
           <Route path="/post/:id" element={<Detail />} />
-          <Route path="/loginForm" element={<LoginForm />} />
-          <Route path="/joinForm" elment={<JoinForm />} />
-          <Route path="/updateForm/:id" element={<UpdateForm />} />
+          <Route path="/loginForm"element={<LoginForm />} />
+          <Route path="/joinForm" element={<JoinForm />} />
+          <Route path="/updateForm/:id"
+                                  element={<UpdateForm />} />
         </Routes>
       </Container>
     </div>
--- src/main/webapp/frontapp/src/pages/user/JoinForm.js
+++ src/main/webapp/frontapp/src/pages/user/JoinForm.js
@@ -1,11 +1,78 @@
-import React from 'react';
+import axios from "axios";
+import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import { useNavigate, useParams } from "react-router-dom";
 
-function JoinForm(props) {
+const JoinForm = (props) => {
+    const navigate = useNavigate();
+
+    const [user, setUser] = useState({
+        username: "",
+        password: "",
+        email   : "",
+    });
+    
+    function changeValue(e){
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    async function submitJoin(e){
+        e.preventDefault(); // action 막기
+        let response = axios({
+            url     : "http://localhost:8080/join",
+            method  : "POST",
+            headers : {
+                'Content-Type'  : 'application/json; charset=utf-8'
+            },
+            data    : user
+        });
+
+        if(response.status === 200){
+            navigate("/loginForm");
+        }else{
+            alert(response.data.msg);
+        }
+    }
+
     return (
-        <div>
-            
-        </div>
+        <Form>
+        <Form.Group>
+            <Form.Label>Username</Form.Label>
+            <Form.Control
+                type="text"
+                placeholder="Enter username"
+                name="username"
+                onChange={changeValue}
+            />
+        </Form.Group>
+
+        <Form.Group>
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+                type="password"
+                placeholder="Enter password"
+                name="password"
+                onChange={changeValue}
+            />
+        </Form.Group>
+
+        <Form.Group>
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+                type="email"
+                placeholder="Enter email"
+                name="email"
+                onChange={changeValue}
+            />
+        </Form.Group>
+        <Button variant="primary" type="submit" onClick={submitJoin}>
+            회원가입
+        </Button>
+        </Form>
     );
-}
+};
 
 export default JoinForm;
